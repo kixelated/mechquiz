6f65393bd78c7aeca54a52429bb869dd0aadd31a
Luke Curley <qpingu@gmail.com>
2014-07-27 21:08:45 -0700

    Progress.

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,23 @@
+(function() {
+  $(function() {
+    var itemReq = $.getJSON('cache/www.dota2.com/jsfeed/itemdata');
+    var abilityReq = $.getJSON('cache/www.dota2.com/jsfeed/abilitydata');
+    var quizReq = $.getJSON('data/quiz.json');
 
+    var allReq = $.when(itemReq, abilityReq, quizReq);
+
+    allReq.done(function(itemRes, abilityRes, quizRes) {
+      var items = itemRes[0].itemdata;
+      var abilities = abilityRes[0].abilitydata;
+      var questions = quizRes[0].questions;
+
+      _.each(questions, function(question) {
+
+      });
+    });
+
+    allReq.fail(function() {
+      alert("failed to load dependencies, please refresh the page");
+    });
+  });
+})();
