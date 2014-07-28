1105db4089d4e0c2178889018725c8cc44c75767
Luke Curley <qpingu@gmail.com>
2014-07-28 00:05:44 -0700

    Animations in place, now time to keep score.

--- static/js/main.js
+++ static/js/main.js
@@ -1,87 +1,105 @@
 (function() {
-  $(function() {
-    var itemReq = $.getJSON('cache/www.dota2.com/jsfeed/itemdata');
-    var abilityReq = $.getJSON('cache/www.dota2.com/jsfeed/abilitydata');
-    var quizReq = $.getJSON('data/quiz.json');
+  var questions, abilities, items;
 
-    var allReq = $.when(itemReq, abilityReq, quizReq);
+  var finish = function() {
+    $("#score").delay(2000).animate({ "line-height": "300px" }, 500);
+  };
 
-    allReq.done(function(itemRes, abilityRes, quizRes) {
-      var items = itemRes[0].itemdata;
-      var abilities = abilityRes[0].abilitydata;
-      var questions = quizRes[0].questions;
+  var next = function() {
+    var question = questions.pop();
+    if (!question) {
+      return finish();
+    }
 
-      var next = function() {
-        var question = questions.pop();
+    var dom = $("<div>").addClass("question").prependTo("#questions");
+    dom.slideUp().slideDown(500);
 
-        var dom = $("<div>").addClass("question").prependTo("#questions");
-        dom.slideUp().slideDown(500);
+    var textNode;
 
-        if (!question) {
-          dom.addClass("score").text("Final score: 42");
-          return;
+    _.each(question.options, function(option) {
+      var temp = option.split("/");
+      var type = temp[0], name = temp[1];
+
+      if (type == "item") {
+        var wdom = $("<div>").appendTo(dom);
+        var odom = $("<div>").appendTo(wdom).addClass("option icon");
+        var cdom = $("<div>").appendTo(wdom).addClass("caption");
+
+        odom.css("background-image", "url('http://cdn.dota2.com/apps/dota2/images/items/" + name + "_lg.png')");
+        cdom.text(items[name].dname);
+
+        if (option == question.answer) {
+          odom.addClass("answer");
+        }
+      } else if (type == "ability") {
+        var wdom = $("<div>").appendTo(dom);
+        var odom = $("<div>").appendTo(wdom).addClass("option icon");
+        var cdom = $("<div>").appendTo(wdom).addClass("caption");
+
+        odom.css("background-image", "url('http://cdn.dota2.com/apps/dota2/images/abilities/" + name + "_hp1.png')");
+        cdom.text(abilities[name].dname);
+
+        if (option == question.answer) {
+          odom.addClass("answer");
+        }
+      } else if (type == "text") {
+        if (!textNode) {
+          textNode = $("<div>").appendTo(dom);
         }
 
-        var textNode;
+        var odom = $("<div>").appendTo(textNode).addClass("option text");
+        odom.text(name);
 
-        _.each(question.options, function(option) {
-          var temp = option.split("/");
-          var type = temp[0], name = temp[1];
+        if (option == question.answer) {
+          odom.addClass("answer");
+        }
+      }
+    });
 
-          if (type == "item") {
-            var wdom = $("<div>").appendTo(dom);
-            var odom = $("<div>").appendTo(wdom).addClass("option icon");
-            var cdom = $("<div>").appendTo(wdom).addClass("caption");
+    var answered = false;
 
-            odom.css("background-image", "url('http://cdn.dota2.com/apps/dota2/images/items/" + name + "_lg.png')");
-            cdom.text(items[name].dname);
+    $(".option", dom).click(function(event) {
+      var elem = $(event.target);
 
-            if (option == question.answer) {
-              odom.addClass("answer");
-            }
-          } else if (type == "ability") {
-            var wdom = $("<div>").appendTo(dom);
-            var odom = $("<div>").appendTo(wdom).addClass("option icon");
-            var cdom = $("<div>").appendTo(wdom).addClass("caption");
+      if (answered) return;
+      answered = true;
 
-            odom.css("background-image", "url('http://cdn.dota2.com/apps/dota2/images/abilities/" + name + "_hp1.png')");
-            cdom.text(abilities[name].dname);
+      if (elem.hasClass("answer")) {
+        elem.addClass("correct");
+      } else {
+        elem.addClass("incorrect");
+        $(".option.answer", dom).addClass("correct");
+      }
 
-            if (option == question.answer) {
-              odom.addClass("answer");
-            }
-          } else if (type == "text") {
-            if (!textNode) {
-              textNode = $("<div>").appendTo(dom);
-            }
+      dom.delay(2000).fadeOut(500, dom.remove);
+      next();
+    });
+  };
 
-            var odom = $("<div>").appendTo(textNode).addClass("option text");
-            odom.text(name);
+  var start = function() {
+    $(".difficulty").click(function(event) {
+      var diff = $(event.target).data("value");
 
-            if (option == question.answer) {
-              odom.addClass("answer");
-            }
-          }
-        });
-
-        $(".option", dom).click(function(event) {
-          var elem = $(event.target);
-
-          if (elem.hasClass("answer")) {
-            elem.addClass("correct");
-          } else {
-            elem.addClass("incorrect");
-            $(".option.answer", dom).addClass("correct");
-          }
-
-          dom.delay(5000).fadeOut(500, dom.remove);
-          setTimeout(next, 500);
-        });
-
-        console.log(question);
-      };
+      $("#instructions").fadeOut(500);
+      $("#score").slideDown(500);
 
       next();
+    });
+  };
+
+  $(function() {
+    var quizReq = $.getJSON('data/quiz.json');
+    var itemReq = $.getJSON('cache/www.dota2.com/jsfeed/itemdata');
+    var abilityReq = $.getJSON('cache/www.dota2.com/jsfeed/abilitydata');
+
+    var allReq = $.when(quizReq, itemReq, abilityReq);
+
+    allReq.done(function(quizRes, itemRes, abilityRes) {
+      questions = quizRes[0].questions;
+      items = itemRes[0].itemdata;
+      abilities = abilityRes[0].abilitydata;
+
+      start();
     });
 
     allReq.fail(function() {
